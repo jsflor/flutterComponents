677d889d37bf3536d364990cd075ca003e3b1646
sebastianflor <sebastian-flor@hotmail.com>
2020-03-18 21:19:02 +0100

    Alerts

--- lib/src/pages/AlertPage.dart
+++ lib/src/pages/AlertPage.dart
@@ -7,6 +7,15 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Alerta"),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _showAlert(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_left),
         onPressed: (){
@@ -15,4 +24,37 @@ class AlertPage extends StatelessWidget {
       ),
     );
   }
+
+  void _showAlert(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular((20.0))),
+          title: Text("Titulo"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text("Content of the box"),
+              FlutterLogo(size: 100.0)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Cancelar"),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text("Ok"),
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      }
+    );
+  }
+
 }
