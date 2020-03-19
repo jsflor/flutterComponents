97cd5d40e6e5658b2d390a6773c3c7e75e69d431
sebastianflor <sebastian-flor@hotmail.com>
2020-03-19 21:12:10 +0100

    Circle Avatar

--- lib/src/pages/AvatarPage.dart
+++ lib/src/pages/AvatarPage.dart
@@ -6,6 +6,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://i.pinimg.com/originals/fa/8a/5e/fa8a5e96b89a7c10fac879658694eac7.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text("SL"),
+              backgroundColor: Colors.purple,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage("https://i.pinimg.com/originals/fa/8a/5e/fa8a5e96b89a7c10fac879658694eac7.jpg"),
+          placeholder: AssetImage('assets/original.gif'),
+          fadeInDuration: Duration( milliseconds: 200),
+        ),
       ),
     );
   }
